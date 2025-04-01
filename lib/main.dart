5a69fb4736dbfef2b7601687f9a0128830465fd7
Renz Gabriel E. Salas <124060701+RenzGabrielSalas@users.noreply.github.com>
2025-04-01 16:43:58 +0800

    Add loading property to be used for CupertinoActivitiyIndicator

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:inventory_api/pages/profile.dart';
 import 'package:inventory_api/pages/users/index.dart';
 import 'package:inventory_api/pages/login.dart';
 
+import 'package:inventory_api/pages/profile.dart';
+
 bool isLoggedIn = false;
+bool isLoading = false;
 
 void main() {
   runApp(CupertinoApp(
-    // home: isLoggedIn ? Indexpage() : Loginpage(),
-    home: Indexpage(),
+    home: isLoggedIn ? Indexpage() : Loginpage(),
     debugShowCheckedModeBanner: false,
-    theme: CupertinoThemeData(brightness: Brightness.light),
+    theme: CupertinoThemeData(
+      brightness: Brightness.light,
+      primaryColor: CupertinoColors.systemOrange
+    ),
+    routes: {
+      Profilepage.routeName: (context) => Profilepage(),
+    },
   ));
 }
